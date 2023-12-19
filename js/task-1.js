355053dfda41cef3fc9517d47e710076a7cb0302
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2023-12-19 21:43:44 +0100

    js 3 1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function slugify(title) {
+  let array = title.toLowerCase().split(" ");
+  let slug = array.join("-");
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
